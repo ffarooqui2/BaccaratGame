a406b2d63a795044d869ad3ae3762b663171cb3c
Hamza-developer1 <mshai9@uic.edu>
2023-10-26 09:13:39 -0500

    Hello Faaiz

--- src/main/java/BaccaratGame.java
+++ src/main/java/BaccaratGame.java
@@ -41,6 +41,7 @@ public class BaccaratGame extends Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Hello Faaiz");
 
 		BaccaratDealer dealer = new BaccaratDealer();
 		System.out.println(dealer.deckSize());
